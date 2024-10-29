f9bb73db68c840e355ad6bfa762b041fa2a65567
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-29 15:44:33 +0600

    active routes, loading spinner , useLocation, useNavigation

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import './Header.css'
 export default function Header() {
   return (
     <div>
         <nav >
-            <Link style={{'margin-right':'20px'}} to="/">Home</Link>
-            <Link style={{'margin-right':'20px'}} to="/about">About</Link>
-            <Link style={{'margin-right':'20px'}} to="/contact">Contact</Link>
-            <Link style={{'margin-right':'20px'}} to="/users">Users</Link>
-            <Link style={{'margin-right':'20px'}} to="/posts">Posts</Link>
+            <NavLink style={{'margin-right':'20px'}} to="/">Home</NavLink>
+            <NavLink style={{'margin-right':'20px'}} to="/about">About</NavLink>
+            <NavLink style={{'margin-right':'20px'}} to="/contact">Contact</NavLink>
+            <NavLink style={{'margin-right':'20px'}} to="/users">Users</NavLink>
+            <NavLink style={{'margin-right':'20px'}} to="/posts">Posts</NavLink>
         </nav>
     </div>
   )
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,15 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 export default function Home() {
+  const navigation = useNavigation();
+  const location = useLocation()
+  console.log(location);
   return (
     <div style={{ 'margin-left':'50px'}}>
         <Header></Header>
         <h3>Hii welcome HOME</h3>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus eum laboriosam ipsum accusantium perspiciatis quia.</p>
         
-        <Outlet></Outlet>
+        {
+          navigation.state === 'loading' ? <p>loading...</p>:<Outlet></Outlet>
+        }
+       
+        
        
         <Footer></Footer>
     </div>
